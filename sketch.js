000a25d689ec721dfd94f6c5ef45709eef1038c8
MICAELA_SOCHA <ms22bm@fsu.edu>
2024-11-04 11:30:23 -0500

    more items

--- sketch.js
+++ sketch.js
@@ -1,11 +1,13 @@
 let backgroundImg;
-let eggsImg, breadImg, juiceImg;
-let eggs, bread, juice;
+let eggsImg, breadImg, juiceImg, milkImg, listImg;
+let eggs, bread, juice, milk, list;
 
 function preload() {
   eggsImg = loadImage("assets/eggs.png");
   breadImg = loadImage("assets/bread.png");
   juiceImg = loadImage("assets/juice.png");
+  milkImg = loadImage("assets/milk.png");
+  listImg = loadImage("assets/list.png");
   backgroundImg = loadImage("assets/shelves.png");
 }
 
@@ -19,13 +21,24 @@ function setup() {
 
   bread = new Sprite();
   bread.img = breadImg;
-  bread.position.set(200, 600); 
+  bread.position.set(200, 1200); 
   bread.drag = 10; 
 
   juice = new Sprite();
   juice.img = juiceImg;
   juice.position.set(200, 900); 
   juice.drag = 10; 
+
+  milk = new Sprite();
+  milk.img = milkImg;
+  milk.position.set(200, 580); 
+  milk.drag = 10; 
+
+  list = new Sprite();
+  list.img = listImg;
+  list.position.set(200, 580);
+
+  
 }
 
 function draw() {
@@ -43,5 +56,9 @@ function draw() {
   
   if (juice.mouse.dragging()) {
     juice.moveTowards(mouseX, mouseY, 1);
+  }
+
+  if (milk.mouse.dragging()) {
+    milk.moveTowards(mouseX, mouseY, 1);
   }
 }
